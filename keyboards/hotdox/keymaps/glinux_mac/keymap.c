a6d02c559c5efe2c155e9f650529d5da0db19380
hiko <hikoship@gmail.com>
2019-05-31 00:42:59 -0700

    Added KBD6x

--- keyboards/hotdox/keymaps/glinux_mac/keymap.c
+++ keyboards/hotdox/keymaps/glinux_mac/keymap.c
@@ -2,31 +2,6 @@
 
 // #undef IGNORE_MOD_TAP_INTERRUPT
 
-bool space_other_key_pressed = false;
-uint16_t space_timer = 0;
-
-enum custom_keycodes {
-  SPACE_GLINUX = MO(1),
-  SPACE_MAC = MO(9),
-};
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case SPACE_GLINUX:
-    case SPACE_MAC:
-      if (record->event.pressed) {
-		space_timer = timer_read();
-		space_other_key_pressed = false;
-		// register_code(MO(9));
-      } else if (timer_elapsed(space_timer) < 500 && !space_other_key_pressed) {
-		tap_code(KC_SPC);
-	  }
-      break;
-	default:
-	  space_other_key_pressed = true;
-  }
-  return true;
-};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_ergodox(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_MINS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC, KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_GRV, MO(3), KC_LCTL, KC_LGUI, KC_LALT, MO(1), KC_LEFT, KC_RGHT, KC_UP, LT(1,KC_SPC), MO(1), KC_DOWN, KC_EQL, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSLS, KC_RBRC, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, MO(1), KC_RCTL, KC_RGUI, KC_RALT, KC_GRV, KC_VOLD, KC_VOLU, KC_PGUP, KC_PGDN, MO(2), KC_ENT),
